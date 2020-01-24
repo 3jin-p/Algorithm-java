c7072d1503e90214199bccb7c283e6b3e95a760e
sj <seajin2003@gmail.com>
2020-01-24 21:10:45 +0900

    BubbleSort

---
+++ src/main/java/BasicAlgorithm/Sort/BubbleSort/Main.java
@@ -0,0 +1,38 @@
+package BasicAlgorithm.Sort.BubbleSort;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] unSortedArray = {5,1,4,9,3,2,7,6,8,10};
+        System.out.println(Arrays.toString(bubbleSort(unSortedArray)));
+    }
+
+    /*
+    버블 정렬은 붙어있는 두 원자 간의 크기를 비교하여
+    서로 자리를 바꿔주며 정렬하는 기법이다.
+     */
+    public static int[]  bubbleSort(int[] array) {
+
+            for (int i = 0; i < array.length; i++) {
+                for (int j = i+1; j < array.length; j++) {
+                    if (array[i] > array[j]) {
+                        swap(i, j, array);
+                    }
+                }
+            }
+
+        return array;
+    }
+
+    /*
+    두 인자들의 인덱스 교환
+     */
+    private static int[] swap(int index_1, int index_2, int[] array) {
+        int temp = array[index_1];
+        array[index_1] = array[index_2];
+        array[index_2] = temp;
+
+        return array;
+    }
+}
---
+++ src/main/java/BasicAlgorithm/Sort/BubbleSort/readme.md
@@ -0,0 +1,16 @@
+## 정렬
+
+Bubble Sort [거품 정렬]
+
+>Logic
+1. index번째 index+1 번째 인자 두개를 비교한다
+2. index번째 인자가 더 크다면 둘의 자리를 바꾼다.
+3. index 를 하나 증가시키며 index가 n-1 될 때까지 반복한다.
+4. 위 과정을 n번 반복한다.
+
+
+![버블정렬](BubbleSort.png)
+
+>시간복잡도
+
+O(N^2) 
