91c12e5538d19189a7ac68effcc40b8a675a2af2
sejin <3jin.pk@gmail.com>
2020-09-17 09:04:47 +0900

    B_11060 ..ing

---
+++ src/main/java/BaekJoon/B_11060/Main.java
@@ -0,0 +1,52 @@
+package BaekJoon.B_11060;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.stream.IntStream;
+
+/**
+ * <b>파일 설명</b>
+ *
+ * @author sejinpark
+ * @since 2020/09/16
+ */
+public class Main {
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static int len;
+    private static int[] maze;
+    private static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solution();
+    }
+
+    private static void input() throws IOException {
+        len = Integer.parseInt(br.readLine());
+        String[] input = br.readLine().split(" ");
+        maze = new int[len];
+        dp = new int[len];
+        maze = Arrays.stream(input)
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        Arrays.fill(dp, -1);
+    }
+
+    private static void solution() {
+        for(int i = 0; i < len; i++) {
+            for(int j = 1 ; j < maze[i] ; j++){
+                if(i+j < len){
+                    if(dp[i + j] == -1) {
+                        dp[i + j] = dp[i] + 1;
+                    }else {
+                        dp[i + j] = Math.min(dp[i+j], dp[i] + 1);
+                    }
+                }
+            }
+        }
+    }
+}
